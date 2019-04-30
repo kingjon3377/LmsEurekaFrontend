5bb6d5b6ee9eabf41c504eb0c0e7e02ec7241539
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-30 17:02:43 -0400

    Give login-failure feedback via an in-form message, not console.log().

--- js/authentication.js
+++ js/authentication.js
@@ -3,25 +3,20 @@ if (!localStorage.getItem("users")) {
 		username: "jonathan", pwd: "correctHorseBatteryStaple"}]));
 }
 function authenticate() {
-	console.log("In authenticate()");
 	const users = JSON.parse(localStorage.getItem("users"));
 	const uname = document.getElementById("username").value;
 	const pwd = document.getElementById("password").value;
 	if (uname === "admin" && pwd === "adminpassword") {
-		console.log("Authenticated with back-door");
 		localStorage.setItem("logged-in-user", "Administrator");
 		return true;
 	} else {
 		for (const user of users) {
 			if (user.username === uname && user.pwd === pwd) {
-				console.log("Authenticated as " + user.username);
 				localStorage.setItem("logged-in-user", user.name);
 				return true;
-			} else if (user.username === uname) {
-				console.log("Known username, incorrect password");
 			}
 		}
-		console.log("Failed to authenticate");
+		document.getElementById("errorMessageDiv").classList.remove("errorMessage");
 		return false;
 	}
 }
