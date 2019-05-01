140d878b6ba74fb65193a3e715a30e3de447f6bf
Jonathan Lovelace <kingjon3377@gmail.com>
2019-05-01 18:44:35 -0400

    Store books entered via booksAdd.html in localStorage.

--- js/books.js
+++ js/books.js
@@ -1,4 +1,5 @@
 // Note that getBooks(), which every other method below calls, requires getAuthor() and getPublisher()
+// Also, validateAndAddBook() requires validateBook().
 if (!localStorage.getItem("books")) {
 	localStorage.setItem("books", JSON.stringify([{"pubId": 3, "title": "The Curse of Chalion", "authId": 1, "bookId": 2}, {"pubId": 5, "title": "Persuasion", "authId": 6, "bookId": 3}, {"pubId": 4, "title": "The Blue Sword", "authId": 5, "bookId": 4}, {"pubId": null, "title": "A Year In Verse", "authId": 13, "bookId": 5}, {"pubId": 6, "title": "The Lost Tribe", "authId": 14, "bookId": 6}, {"pubId": 3, "title": "Paladin of Souls", "authId": 1, "bookId": 7}]));
 }
@@ -45,6 +46,37 @@ function saveBook(book) {
 		buffer.push(simplify(book));
 	}
 	localStorage.setItem("books", JSON.stringify(buffer));
+}
+function getNextBookId() {
+	let highestBookId = parseInt(localStorage.getItem("highestBookId"), 10);
+	if (highestBookId) {
+		localStorage.setItem("highestBookId", 1 + highestBookId);
+		return highestBookId + 1;
+	} else {
+		highestBookId = 0;
+		for (book of getBooks()) {
+			if (book.bookId > parseInt(highestBookId)) {
+				highestBookId = Number(book.bookId);
+			}
+		}
+		localStorage.setItem("highestBookId", highestBookId + 1);
+		return highestBookId + 1;
+	}
+}
+function validateAndAddBook() {
+	if (validateBook()) {
+		const authorNode = document.getElementById("bookAuthor");
+		const publisherNode = document.getElementById("bookPublisher");
+		saveBook({
+			title: document.getElementById("bookTitle").value,
+			authId: authorNode.options[authorNode.selectedIndex].value,
+			pubId: publisherNode.options[publisherNode.selectedIndex].value,
+			bookId: getNextBookId()
+		});
+		return true;
+	} else {
+		return false;
+	}
 }
 function deleteBook(book) {
 	const original = getBooks();
